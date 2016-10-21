f9ff7738ca916d6f8430efbdb30d90996412f915
wangkun <845885222@qq.com>
2016-10-21 23:38:58 +0800

    ADD#添加单元测试

---
+++ book-facility/src/test/java/com/lockbur/book/test/dao/ChatMessageDaoTest.java
@@ -0,0 +1,36 @@
+package com.lockbur.book.test.dao;
+
+
+import com.lockbur.book.dao.ChatMessageDao;
+import com.lockbur.book.domain.ChatMessageEntity;
+import com.lockbur.book.test.TestBase;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.annotation.Resource;
+
+/**
+ * 聊天DAO测试
+ * Created by wangkun23 on 2016/7/13.
+ */
+public class ChatMessageDaoTest extends TestBase {
+    private static final Logger LOGGER = LoggerFactory.getLogger(ChatMessageDaoTest.class);
+
+    @Resource
+    private ChatMessageDao chatMessageDao;
+
+    @Test
+    public void insert() {
+
+        ChatMessageEntity message=new ChatMessageEntity();
+
+        message.setRank("2");
+        message.setUid("admin");
+        message.setText("ChatMessageEntity");
+        message.setRoomId("7383");
+        chatMessageDao.insert(message);
+        LOGGER.info("##########");
+    }
+
+}
