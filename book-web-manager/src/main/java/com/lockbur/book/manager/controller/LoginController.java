df66912aa4427fe2c8572958b0aba503bea190e5
wangkun <845885222@qq.com>
2016-10-22 09:36:06 +0800

    ADD#Spring  mvc 测试

--- book-web-manager/src/main/java/com/lockbur/book/manager/controller/LoginController.java
+++ book-web-manager/src/main/java/com/lockbur/book/manager/controller/LoginController.java
@@ -1,8 +1,23 @@
 package com.lockbur.book.manager.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+
 /**
  * Created by Administrator on 2016/10/21.
  */
+@Controller
+@RequestMapping("/")
 public class LoginController {
+    private final Logger logger = LoggerFactory.getLogger(getClass());
 
+    @RequestMapping(value = "login", method = RequestMethod.GET)
+    public String displayLogin() {
+        logger.info("displayLogin");
+        return "list";
+    }
 }
