6b5d36a6c545c99f67002c3a8b2b6eb9f91b634b
wangkun23 <wang.kun@jinyinwu.com>
2017-02-10 18:08:41 +0800

    ADD#Spring security Test

--- book-gateway/src/main/java/com/lockbur/book/gateway/service/EmailService.java
+++ book-gateway/src/main/java/com/lockbur/book/gateway/service/EmailService.java
@@ -9,6 +9,29 @@ import java.util.Map;
  */
 public interface EmailService {
 
+    /**
+     * @param toMail
+     * @param subject
+     * @param templateName
+     * @param data
+     * @throws MessagingException
+     * @throws UnsupportedEncodingException
+     */
     public void send(String toMail, String subject, String templateName, Map<String, Object> data)
             throws MessagingException, UnsupportedEncodingException;
+
+    /**
+     * @param smtpFromMail
+     * @param smtpHost
+     * @param smtpPort
+     * @param smtpUsername
+     * @param smtpPassword
+     * @param toMail
+     * @param subject
+     * @param templateName
+     * @param data
+     */
+    public void send(String smtpFromMail, String smtpHost, Integer smtpPort, String smtpUsername,
+                     String smtpPassword, String toMail, String subject, String templateName,
+                     Map<String, Object> data);
 }
--- book-service/src/main/java/com/lockbur/book/service/impl/EmailServiceImpl.java
+++ book-service/src/main/java/com/lockbur/book/service/impl/EmailServiceImpl.java
@@ -2,7 +2,9 @@ package com.lockbur.book.service.impl;
 
 import com.lockbur.book.gateway.service.EmailService;
 import org.apache.velocity.app.VelocityEngine;
-import org.springframework.mail.javamail.JavaMailSender;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 import org.springframework.ui.velocity.VelocityEngineUtils;
@@ -18,9 +20,10 @@ import java.util.Map;
 @Service
 public class EmailServiceImpl implements EmailService {
 
+    final Logger LOGGER = LoggerFactory.getLogger(EmailServiceImpl.class);
 
     @Resource
-    private JavaMailSender mailSender;
+    private JavaMailSenderImpl mailSender;
 
     @Resource
     private VelocityEngine velocityEngine;
@@ -46,5 +49,38 @@ public class EmailServiceImpl implements EmailService {
         mailSender.send(mailMessage);
     }
 
+
+    public void send(String smtpFromMail, String smtpHost, Integer smtpPort, String smtpUsername, String smtpPassword, String toMail, String subject, String templateName, Map<String, Object> data) {
+
+        Assert.hasText(smtpFromMail);
+        Assert.hasText(smtpHost);
+        Assert.notNull(smtpPort);
+        Assert.hasText(smtpUsername);
+        Assert.hasText(smtpPassword);
+        Assert.hasText(toMail);
+        Assert.hasText(subject);
+        Assert.hasText(templateName);
+
+        //读取邮件模板
+        String text = VelocityEngineUtils.mergeTemplateIntoString(velocityEngine, templateName, "UTF-8", data);
+
+        mailSender.setHost(smtpHost);
+        mailSender.setPort(smtpPort);
+        mailSender.setUsername(smtpUsername);
+        mailSender.setPassword(smtpPassword);
+        try {
+            MimeMessage mailMessage = mailSender.createMimeMessage();
+            MimeMessageHelper messageHelper = new MimeMessageHelper(mailMessage, false, "utf-8");
+
+            messageHelper.setFrom(MimeUtility.encodeWord("系统邮件") + " <wang.kun@zhongfl.com>");
+            messageHelper.setTo(toMail);
+            messageHelper.setSubject(subject);
+            messageHelper.setText(text, true);
+
+            mailSender.send(mailMessage);
+        } catch (MessagingException | UnsupportedEncodingException ex) {
+            LOGGER.error("send email error {}", ex);
+        }
+    }
 
 }
